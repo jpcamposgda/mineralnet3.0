223f59659c15c2010d6e5dca22c9508c08f983df
João pedro da silva campos <jpcamposgda@gmail.com>
2022-05-03 16:57:30 -0400

    commitfirst

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,135 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+   
+
+   
+      
+      <v-nav>
+        <v-img 
+          :src="require('../../img/Group 1.svg')"
+          class="group1"
+        />
+
+        </v-nav>
+       
+        <div class="mx-8">
+          
+         <span class="label">Documento (CPF/CNPJ)</span>
+         
+         <input type="text" class="input " placeholder="informe seu documento">
+         
+         <span class="label">Senha</span>
+         
+         <input type="text" class="input" placeholder="informe sua senha">
+
+         <span> <a class="link" href="">Esqueci minha senha</a> </span>
+
+        </div>
+
+        
+     
+         
+      
+        
+   
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+
+  
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+/* Group 1 */
+
+.label{
+
+position: static;
+width: 231px;
+height: 20px;
+left: 0px;
+top: 0px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 20px;
+/* identical to box height, or 143% */
+
+
+color: #4F4F4F;
+
+opacity: 0.5;
+
+
+
+
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+
+.input{
+
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 16px;
+
+position: static;
+width: 291px;
+height: 40px;
+left: 24px;
+top: 36px;
+
+background: #F8F8F8;
+border: 2px solid #E8E8E8;
+box-sizing: border-box;
+border-radius: 6px;
+
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.link{
+  position: static;
+width: 280px;
+height: 16px;
+left: 24px;
+top: 0px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 16px;
+/* identical to box height, or 133% */
+
+text-align: right;
+
+color: #B4771C;
 }
-a {
-  color: #42b983;
+
+.bigcaixa{
+
+
+
+
 }
+
+.color{
+
+background: var(--sinza-principal);
+
+}
+
+
+:root{
+
+--sinza-principal: #B4771C;
+
+}
+
 </style>
