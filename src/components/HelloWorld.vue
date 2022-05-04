08634101b88232ff1719dfc0ff05b552548bfccf
João pedro da silva campos <jpcamposgda@gmail.com>
2022-05-04 14:54:44 -0400

    layout login completo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,26 +5,35 @@
       
       <v-nav>
         <v-img 
-          :src="require('../../img/Group 1.svg')"
+          :src="require('../../img/Group 1.png')"
           class="group1"
         />
-
+<img src="" alt="">
         </v-nav>
        
-        <div class="mx-8">
+        <div class=" centralizar   mt-14">
           
          <span class="label">Documento (CPF/CNPJ)</span>
          
-         <input type="text" class="input " placeholder="informe seu documento">
+         <input type="email" class="input mb-7 	 input-text " placeholder="Informe seu documento">
          
          <span class="label">Senha</span>
          
-         <input type="text" class="input" placeholder="informe sua senha">
-
-         <span> <a class="link" href="">Esqueci minha senha</a> </span>
-
+         <input type="password" class="input input-text " placeholder="Informe sua senha" required  >
+         
+         
+        <div class="text-right my-5 "> <span > <a class="link"   href="">Esqueci minha senha</a> </span></div>
+         
+         
         </div>
-
+<div class=" mt-16   centralizar">
+  <v-btn type="submit"  class="buttons1  mt-6 px-4" > Entrar </v-btn>
+  
+  <v-btn type="submit"  class="  mt-6 px-4 buttons " >Quero me cadastrar</v-btn>
+   
+  
+  
+</div>
         
      
          
@@ -43,6 +52,12 @@ export default {
 </script>
 
 <style scoped>
+
+
+.centralizar{
+  width: 328px;
+  margin: 0 auto;
+}
 
 /* Group 1 */
 
@@ -74,14 +89,15 @@ opacity: 0.5;
 
 .input{
 
+width: 100%;
 display: flex;
 flex-direction: row;
 justify-content: center;
 align-items: center;
-padding: 16px;
+padding: 16px    ;
 
 position: static;
-width: 291px;
+
 height: 40px;
 left: 24px;
 top: 36px;
@@ -90,6 +106,27 @@ background: #F8F8F8;
 border: 2px solid #E8E8E8;
 box-sizing: border-box;
 border-radius: 6px;
+
+}
+
+.input-text{
+/* Digite... */
+
+
+position: static;
+
+
+font-family: 'Ubuntu';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 100%;
+/* identical to box height, or 14px */
+
+
+color: #0A0A0A;
+
+
 
 }
 
@@ -105,31 +142,109 @@ font-style: normal;
 font-weight: 400;
 font-size: 12px;
 line-height: 16px;
+
+text-decoration: none;
 /* identical to box height, or 133% */
 
-text-align: right;
+
 
 color: #B4771C;
 }
 
-.bigcaixa{
+.link:hover{
+
+  text-decoration: underline;
+}
+
+.buttons1{
+/* Bottom */
 
 
+/* Auto layout */
+
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 0px 16px;
+
+
+width: 100%;
+height: 44px;
+/* left: 35.5px; */
+top: 0px;
+
+background: #B4771C;
+border-radius: 8px;
+font-family: 'Inter';
+font-style: normal;
+font-weight: 700;
+font-size: 14px;
+line-height: 20px;
+/* identical to box height, or 143% */
+
+display: flex;
+align-items: center;
+text-align: center;
+
+color: #F8F8F8;
+
+
+
+text-transform: initial;
 
 
 }
 
-.color{
 
-background: var(--sinza-principal);
+
+
+
+.buttons{
+/* Bottom */
+
+
+/* Auto layout */
+
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 0px 16px;
+
+
+width: 100%;
+height: 44px;
+/* left: 35.5px; */
+/* top: 68px; */
+
+border: 2px solid #B4771C;
+box-sizing: border-box;
+border-radius: 8px;
+
+/* Inside auto layout */
+
+flex: none;
+order: 1;
+flex-grow: 0;
+margin: 24px 0px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 700;
+font-size: 14px;
+line-height: 20px;
+/* identical to box height, or 143% */
+
+display: flex;
+align-items: center;
+text-align: center;
+
+color: #B4771C;
+text-transform: initial;
 
 }
 
 
-:root{
-
---sinza-principal: #B4771C;
-
-}
 
 </style>
